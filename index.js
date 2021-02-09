7a43fadaa17097828d7fd19b7ad28ff09f20ff1a
Christian Vimu <Christian.Vimu@gmail.com>
2021-02-09 00:59:49 -0300

    creation of js and jquery logic

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
-    alert("Jquery loaded");
+$(document).ready(function () {
+    $("#theme-selector").on("click", function () {
+        let header = $("#header");
+        let tDay = "theme-day";
+        let tNight = "theme-night";
+        if (header.hasClass(tDay)) {
+            let theme = $(".theme-day")
+            theme.each(function (indice, val) {
+                $(val).toggleClass(`${tDay} ${tNight}`);
+            });
+        } else if (header.hasClass(tNight)) {
+            let theme = $(".theme-night")
+            theme.each(function (indice, val) {
+                $(val).toggleClass(`${tNight} ${tDay}`);
+            });
+        } else {
+            console.log("Error theme");
+        }
+    });
 });
