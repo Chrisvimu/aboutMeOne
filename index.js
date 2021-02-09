3a2344ebb90f5077cadad32855ccb4d7745171fb
Christian Vimu <Christian.Vimu@gmail.com>
2021-02-09 04:18:07 -0300

    Index progress, custom navbar changes colour using button

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ $(document).ready(function () {
         let header = $("#header");
         let tDay = "theme-day";
         let tNight = "theme-night";
+        let tImage = document.getElementById('theme-image');
+        let tButton = document.getElementById('theme-selector');
         if (header.hasClass(tDay)) {
             let theme = $(".theme-day")
             theme.each(function (indice, val) {
                 $(val).toggleClass(`${tDay} ${tNight}`);
+                tImage.src = "Resources/moon.png"
+                tButton.className = "btn btn-outline-secondary"
             });
         } else if (header.hasClass(tNight)) {
             let theme = $(".theme-night")
             theme.each(function (indice, val) {
                 $(val).toggleClass(`${tNight} ${tDay}`);
+                tImage.src = "Resources/sun.png"
+                tButton.className = "btn btn-outline-warning"
             });
         } else {
             console.log("Error theme");
