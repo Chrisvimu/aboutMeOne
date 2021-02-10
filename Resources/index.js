5d83d9884283945fa6475f62db76a53daf84e15a
Christian Vimu <Christian.Vimu@gmail.com>
2021-02-09 22:12:22 -0300

    Fxes to load about.html and CSS for about section

--- Resources/index.js
+++ Resources/index.js
@@ -5,23 +5,23 @@ $(document).ready(function () {
         let tNight = "theme-night";
         let tImage = document.getElementById('theme-image');
         let tButton = document.getElementById('theme-selector');
-        let tbody = $("#body");
+        // let tbody = document.getElementById('body');
         if (header.hasClass(tDay)) {
             let theme = $(".theme-day")
             theme.each(function (indice, val) {
                 $(val).toggleClass(`${tDay} ${tNight}`);
-                tImage.src = "Resources/moon.png"
-                tButton.className = "btn btn-outline-secondary"
-                tbody.toggleClass(`${"body-day"} ${"body-night"}`);
+                tImage.src = "Resources/moon.png";
+                tButton.className = "btn btn-outline-secondary";
+                tbody.className = "body-night";
 
             });
         } else if (header.hasClass(tNight)) {
             let theme = $(".theme-night")
             theme.each(function (indice, val) {
                 $(val).toggleClass(`${tNight} ${tDay}`);
-                tImage.src = "Resources/sun.png"
-                tButton.className = "btn btn-outline-warning"
-                tbody.toggleClass(`${"body-night"} ${"body-day"}`);
+                tImage.src = "Resources/sun.png";
+                tButton.className = "btn btn-outline-warning";
+                tbody.className = "body-day";
             });
         } else {
             console.log("Error theme");
